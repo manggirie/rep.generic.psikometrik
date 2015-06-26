efeeaf6f146d493c7d9da506e87fa54d1a799fe6
erymuzuan <erymuzuan@hotmail.com>
2015-06-26 14:55:40 +0800

    Olrait

---
+++ web/SphApp/partial/penyelaras.js
@@ -0,0 +1,16 @@
+define([], function(){
+    var activate = function(list){
+            return true;
+
+
+        },
+        attached  = function(view){
+        
+        };
+
+    return {
+        activate : activate,
+        attached : attached
+    };
+
+});
