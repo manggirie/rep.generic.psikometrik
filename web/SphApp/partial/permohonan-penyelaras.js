f3622546aa1bbfc53afe107634681eb7ef8b4b04
erymuzuan <erymuzuan@hotmail.com>
2015-07-08 06:27:21 +0800

    Kemaskini Bilangan responden perlukan form baru sebab permohonan penyelaras terus pergi ke Operation yang set Status kepada BARU

--- web/SphApp/partial/permohonan-penyelaras.js
+++ web/SphApp/partial/permohonan-penyelaras.js
@@ -1,20 +1,27 @@
 define([objectbuilders.config], function(config){
-    var activate = function(entity){
-            
-            entity.NamaJabatan(config.namaJabatan);
-            entity.NamaKementerian(config.namaKementerian);
-            entity.Penyelaras(config.userName);
-            if(!ko.unwrap(entity.StatusPermohonan)){
-                entity.StatusPermohonan("BARU");
-            }
-            
-            return true;
-
-
-        },
-        attached  = function(view){
-        
-        };
+    var     permohonan = ko.observable(),
+            activate = function(entity){
+                permohonan(entity);
+                entity.NamaJabatan(config.namaJabatan);
+                entity.NamaKementerian(config.namaKementerian);
+                entity.Penyelaras(config.userName);
+                if(!ko.unwrap(entity.StatusPermohonan)){
+                    entity.StatusPermohonan("BARU");
+                }
+                
+                return true;
+    
+    
+            },
+            attached  = function(view){
+                var tarikhMulaPicker = $("#tarikhMulaPicker").data("kendoDateTimePicker"),
+                    tarikhTamatPicker = $("#tarikhTamatPicker").data("kendoDateTimePicker");
+                tarikhMulaPicker.min(new Date());
+                tarikhTamatPicker.min(new Date());
+                
+                tarikhMulaPicker.enable(permohonan().StatusPermohonan() === 'BARU');
+                tarikhTamatPicker.enable(permohonan().StatusPermohonan() === 'BARU');
+            };
 
     return {
         activate : activate,
