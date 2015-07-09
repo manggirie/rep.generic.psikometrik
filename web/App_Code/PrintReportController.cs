d67dd4c09b18bc9b15472c1f563a290fb1c0e9f0
erymuzuan <erymuzuan@hotmail.com>
2015-07-09 08:14:12 +0800

    Laporan Khas HLP

--- web/App_Code/PrintReportController.cs
+++ web/App_Code/PrintReportController.cs
@@ -24,6 +24,33 @@ namespace web.sph.App_Code
     		ConfigHelper.RegisterDependencies();
     	}
 
+
+      	[Route("indikator/hlp/{id}")]
+      	public async Task<ActionResult> HlpIndikator(string id )
+      	{
+      		var context = new SphDataContext();
+      		var sesi = await context.LoadOneAsync<Bespoke.epsikologi_sesiujian.Domain.SesiUjian>(x => x.Id == id);
+          var user = await context.LoadOneAsync<Bespoke.epsikologi_pengguna.Domain.Pengguna>(x => x.MyKad == sesi.MyKad);
+
+          var query = context.CreateQueryable<Bespoke.epsikologi_skorhlp.Domain.SkorHlp>();
+          var lo = await context.LoadAsync(query, size:1000);
+          var scoreTables = lo.ItemCollection;
+
+
+          var rq = context.CreateQueryable<Bespoke.epsikologi_hlprecomendation.Domain.HlpRecomendation>();
+          var rlo = await context.LoadAsync(rq, size:200);
+          var recommendations = rlo.ItemCollection;
+
+
+      		if(null == sesi)
+      			return HttpNotFound("Cannot find SesiUjian " + id);
+        	if(null == sesi)
+        		return HttpNotFound("Cannot find user with MyKad " + sesi.MyKad);
+
+          var vm = new HlpTraitViewModel(sesi, user, scoreTables.ToArray(), recommendations.ToArray());
+      		return View("Indikator-Hlp", vm);
+      	}
+
     	[Route("trait/hlp/{id}")]
     	public async Task<ActionResult> PrintTraitForHlp(string id )
     	{
