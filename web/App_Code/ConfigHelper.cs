203e4105b8d44fb1f14d86cbce6557acc2870b41
erymuzuan <erymuzuan@hotmail.com>
2015-07-30 14:24:10 +0800

    Laporan untuk IP

--- web/App_Code/ConfigHelper.cs
+++ web/App_Code/ConfigHelper.cs
@@ -40,6 +40,9 @@ namespace web.sph.App_Code
             ObjectBuilder.AddCacheList<IRepository<Bespoke.epsikologi_hlprecomendation.Domain.HlpRecomendation>>(
                     new Bespoke.Sph.SqlRepository.SqlRepository<Bespoke.epsikologi_hlprecomendation.Domain.HlpRecomendation>());
 
+            ObjectBuilder.AddCacheList<IRepository<Bespoke.epsikologi_iprecommendation.Domain.IpRecommendation>>(
+                    new Bespoke.Sph.SqlRepository.SqlRepository<Bespoke.epsikologi_iprecommendation.Domain.IpRecommendation>());
+
     	}
     }
 }
