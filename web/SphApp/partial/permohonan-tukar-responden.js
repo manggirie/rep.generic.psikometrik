f7dda3dd1e403c0b3d0a2f552bae8c7192fbf4be
erymuzuan <erymuzuan@hotmail.com>
2015-08-05 09:56:04 +0800

    disable the save button if the TarikhTamat has elapsed

--- web/SphApp/partial/permohonan-tukar-responden.js
+++ web/SphApp/partial/permohonan-tukar-responden.js
@@ -1,5 +1,8 @@
 define([objectbuilders.config], function(config){
-    var     permohonan = ko.observable(),
+    var     permohonan = ko.observable({
+                TarikTamat : ko.observable(moment().format("YYYY-MM-DD"))
+            }),
+            saveTrigger = ko.observable(false),
             activate = function(entity){
                 permohonan(entity);
                 entity.NamaJabatan(config.namaJabatan);
@@ -21,11 +24,29 @@ define([objectbuilders.config], function(config){
                 
                 tarikhMulaPicker.enable(permohonan().StatusPermohonan() === 'BARU');
                 tarikhTamatPicker.enable(permohonan().StatusPermohonan() === 'BARU');
-            };
+                
+                saveTrigger(true);// fire this to re-eval
+                
+              
+            },
+            canExecuteSaveCommand = ko.computed(function(){
+                if(!ko.unwrap(saveTrigger)){
+                    return false;
+                }
+                  var date = moment(),
+                    tarikhTamat = moment(ko.unwrap(permohonan().TarikhTamat));
+                
+                if(date > tarikhTamat){
+                    return false;
+                }
+                
+                return true;
+            });
 
     return {
         activate : activate,
-        attached : attached
+        attached : attached,
+        canExecuteSaveCommand : canExecuteSaveCommand
     };
 
 });
