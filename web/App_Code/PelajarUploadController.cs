4cffd4072f17587ad2c7ddab709e3fba7b84c323
erymuzuan <erymuzuan@hotmail.com>
2015-08-10 08:34:23 +0800

    Upload pelajar guna excel

---
+++ web/App_Code/PelajarUploadController.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Bespoke.epsikologi_pendaftaranprogram.Domain;
+using Bespoke.epsikologi_pengguna.Domain;
+using Bespoke.epsikologi_permohonan.Domain;
+using Bespoke.Sph.Domain;
+using OfficeOpenXml;
+
+namespace web.sph.App_Code
+{
+    [RoutePrefix("pelajar")]
+    [Authorize(Roles = "PengurusanPenggunaJabatan, developers")]
+    public class PelajarUploadController : Controller
+    {
+        public PelajarUploadController()
+        {
+            ConfigHelper.RegisterDependencies();
+        }
+
+        [HttpGet]
+        [Route("test")]
+        public ActionResult Test()
+        {
+            return Content("test OK");
+        }
+
+        [HttpPost]
+        [Route("process-file")]
+        public async Task<ActionResult> ProcessFile(string id, string permohonanId)
+        {
+            if (string.IsNullOrEmpty(id)) return Json(new {success  =false, messge = "Store Id is not provided for the excel file"});
+            var context = new SphDataContext();
+            var store = ObjectBuilder.GetObject<IBinaryStore>();
+            var doc = await store.GetContentAsync(id);
+            var temp = System.IO.Path.GetTempFileName() + ".xlsx";
+            System.IO.File.WriteAllBytes(temp, doc.Content);
+
+            var file = new FileInfo(temp);
+            var excel = new ExcelPackage(file);
+            var ws = excel.Workbook.Worksheets["Pelajar"];
+
+
+            var permohonan = await context.LoadOneAsync<Permohonan>(x => x.Id == permohonanId);
+            if (null == permohonan) return HttpNotFound("Cannot find any Permohonan with Id=" + permohonanId);
+
+            var senaraiDaftar = new List<PendaftaranProgram>();
+            var row = 2;
+            var name = ws.Cells["A" + row].GetValue<string>();
+            var mykad = ws.Cells["B" + row].GetValue<string>();
+            while (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(mykad))
+            {
+                var student = new Pengguna
+                {
+                    Id = mykad,
+                    Nama = name,
+                    MyKad =mykad,
+                    Jantina = ws.Cells["C" + row].GetValue<string>(),
+                    Warganegara = ws.Cells["D" + row].GetValue<string>(),
+                    KumpulanUmur = ws.Cells["E" + row].GetValue<string>(),
+                    Emel = ws.Cells["F" + row].GetValue<string>(),
+                    Emel2 = ws.Cells["G" + row].GetValue<string>(),
+                    Pelajar = new Pelajar
+                    {
+                        Minat = ws.Cells["H" + row].GetValue<string>(),
+                        BidangPengajian = ws.Cells["I" + row].GetValue<string>(),
+                        LuarNegara = ws.Cells["J" + row].GetValue<string>() == "Ya",
+                        TahapPendidikan = ws.Cells["K" + row].GetValue<string>()
+
+                    }
+                };
+
+                var exist = await context.GetAnyAsync<Pengguna>(x => x.MyKad == student.MyKad);
+                var did = Guid.NewGuid().ToString();
+                var daftar = new PendaftaranProgram
+                {
+                    NamaProgram = permohonan.NamaProgram,
+                    MyKad = student.MyKad,
+                    TarikhDaftar = DateTime.Now,
+                    NamaPengguna = student.Nama,
+                    NoPermohonan = permohonan.PermohonanNo,
+                    PendaftaranNo = did,
+                    PermohonanId = permohonanId,
+                    Id = did,
+                    WebId = did
+
+                };
+                senaraiDaftar.Add(daftar);
+                using (var session = context.OpenSession())
+                {
+                    if (!exist)
+                        session.Attach(student);
+                    session.Attach(daftar);
+                    await session.SubmitChanges("import-pelajar");
+                }
+
+                //increment
+                row++;
+                name = ws.Cells["A" + row].GetValue<string>();
+                mykad = ws.Cells["B" + row].GetValue<string>();
+            }
+
+
+
+            return Json(new { success = true, list = senaraiDaftar, status = "OK" });
+        }
+
+    }
+
+}
--- web/SphApp/partial/permohonan-dan-senarai-responden.js
+++ web/SphApp/partial/permohonan-dan-senarai-responden.js
@@ -1,12 +1,12 @@
-define(["services/datacontext", objectbuilders.app, objectbuilders.config, objectbuilders.logger], function(context, app, config, logger) {
-	"use strict"
+define(["services/datacontext", objectbuilders.app, objectbuilders.config, objectbuilders.logger], function (context, app, config, logger) {
+    "use strict";
     var permohonan = ko.observable(),
         filePelajar = ko.observable(),
         senaraiPendaftaran = ko.observableArray(),
-        activate = function(entity){
+        activate = function (entity) {
             permohonan(entity);
             var tcs = new $.Deferred();
-            setTimeout(function(){
+            setTimeout(function () {
                 tcs.resolve(true);
             }, 500);
 
@@ -14,79 +14,79 @@ define(["services/datacontext", objectbuilders.app, objectbuilders.config, objec
 
 
         },
-        attached  = function(view){
-        
+        attached = function () {
+
         },
-        save = function(pendaftaran) {
+        save = function (pendaftaran) {
 
             var data = ko.mapping.toJSON(pendaftaran),
-                sixMonthsAgo = moment().subtract(6, 'M').format("YYYY-MM-DD"),
+                sixMonthsAgo = moment().subtract(6, "M").format("YYYY-MM-DD"),
 				pm = permohonan();
-			
-				var ujian = "";
-				if(ko.unwrap(pm.isIBK)){
-					ujian = "IBK";
-				}
-				if(ko.unwrap(pm.isUKBP)){
-					ujian = "UKBP";
-				}
-				if(ko.unwrap(pm.isIP)){
-					ujian = "IP";
-				}
-				if(ko.unwrap(pm.isHLP)){
-					ujian = "HLP";
-				}
-				if(ko.unwrap(pm.isIOC)){
-					ujian = "IOC";
-				}
-				if(ko.unwrap(pm.isIPE)){
-					ujian = "IPE";
-				}
-				if(ko.unwrap(pm.isIPU)){
-					ujian = "IPU";
-				}
-				if(ko.unwrap(pm.isISO)){
-					ujian = "ISO";
-				}
-				if(ko.unwrap(pm.isISP)){
-					ujian = "ISP";
-				}
-				if(ko.unwrap(pm.isPTD)){
-					ujian = "PTD";
-				}
-			
+
+            var ujian = "";
+            if (ko.unwrap(pm.isIBK)) {
+                ujian = "IBK";
+            }
+            if (ko.unwrap(pm.isUKBP)) {
+                ujian = "UKBP";
+            }
+            if (ko.unwrap(pm.isIP)) {
+                ujian = "IP";
+            }
+            if (ko.unwrap(pm.isHLP)) {
+                ujian = "HLP";
+            }
+            if (ko.unwrap(pm.isIOC)) {
+                ujian = "IOC";
+            }
+            if (ko.unwrap(pm.isIPE)) {
+                ujian = "IPE";
+            }
+            if (ko.unwrap(pm.isIPU)) {
+                ujian = "IPU";
+            }
+            if (ko.unwrap(pm.isISO)) {
+                ujian = "ISO";
+            }
+            if (ko.unwrap(pm.isISP)) {
+                ujian = "ISP";
+            }
+            if (ko.unwrap(pm.isPTD)) {
+                ujian = "PTD";
+            }
+
             return context.loadOneAsync("Permohonan", "Id eq '" + ko.unwrap(pendaftaran.PermohonanId) + "'")
-                .then(function(pm){
+                .then(function (pm) {
                     // TODO : what it has more than 1 Ujian
-                    var query =  String.format("Status eq 'Diambil' and TarikhUjian ge DateTime'{0}' and MyKad eq '{1}' and NamaUjian eq '{2}'", sixMonthsAgo, pendaftaran.MyKad(), ujian);
+                    var query = String.format("Status eq 'Diambil' and TarikhUjian ge DateTime'{0}' and MyKad eq '{1}' and NamaUjian eq '{2}'", sixMonthsAgo, pendaftaran.MyKad(), ujian);
                     return context.getCountAsync("SesiUjian", query, "Id")
                 })
-                .then(function(count){
-                    if(count > 0){
+                .then(function (count) {
+                    if (count > 0) {
                         logger.error(String.format("{0} sudah menduduki ujian {1} dan perlu menunngu 6 bulan", pendaftaran.MyKad(), ujian), ko.toJS(pendaftaran));
-						return Task.fromResult("Sudah daftar");
+                        return Task.fromResult("Sudah daftar");
                     }
                     return context.post(data, "/PendaftaranProgram/TambahResponden");
                 })
-                .then(function(result) {
-					if(result === "Sudah daftar"){
-						return;
-					}
+                .then(function (result) {
+                    if (result === "Sudah daftar") {
+                        return;
+                    }
                     senaraiPendaftaran.push(pendaftaran);
                 });
 
         },
-        addResponden = function(){
+        addResponden = function () {
 
             var tcs = new $.Deferred();
-            require(['viewmodels/tambah-responden-dialog' , 'durandal/app'], function (dialog, app2) {
-                dialog.maxCount ( permohonan().BilRespondan() - senaraiPendaftaran().length);
+            require(["viewmodels/tambah-responden-dialog", "durandal/app"], function (dialog, app2) {
+                dialog.maxCount(permohonan().BilRespondan() - senaraiPendaftaran().length);
                 dialog.senaraiPendaftaran(senaraiPendaftaran());
                 app2.showDialog(dialog)
                     .done(function (result) {
                         if (result === "OK") {
                             console.log(dialog.selectedRespondens());
-                            var registrations = _(dialog.selectedRespondens()).map(function(v){
+                            var registrations = _(dialog.selectedRespondens()).map(function (v) {
                                 var r = new bespoke.epsikologi_pendaftaranprogram.domain.PendaftaranProgram(v);
                                 r.NamaProgram(ko.unwrap(permohonan().NamaProgram));
                                 r.NoPermohonan(ko.unwrap(permohonan().PermohonanNo));
@@ -99,52 +99,56 @@ define(["services/datacontext", objectbuilders.app, objectbuilders.config, objec
                             var tasks = _(registrations).map(save);
                             $.when(tasks).done(tcs.resolve);
 
-                        }else{
+                        } else {
                             tcs.resolve();
                         }
-                });
+                    });
             });
 
             return tcs.promise();
         },
-        sendReminderEmail = function(pendaftaran){
-            return function(){
+        sendReminderEmail = function (pendaftaran) {
+            return function () {
 
-            var to = "";
-            return context.loadOneAsync("Pengguna", String.format("MyKad eq '{0}'", ko.unwrap(pendaftaran.MyKad)))
-                .then(function(user){
-                    to = ko.unwrap(user.Emel);
-                    return $.get("/email-template/generate/Permohonan/" + ko.unwrap(permohonan().Id) + "/peringatan-kepada-responden") ;
-                })
-                .then(function(mail){
-                    var message = {
-                        subject : mail.subject,
-                        body : mail.body,
-                        to : to
-                    };
-                   return context.post(JSON.stringify(message), "/email-template/send");
-                }).then(function(){
-                    app.showMessage("Emel peringatan sudah dihantar kepada " + to, config.applicationFullName, ["OK"]);
-                });
+                var to = "";
+                return context.loadOneAsync("Pengguna", String.format("MyKad eq '{0}'", ko.unwrap(pendaftaran.MyKad)))
+                    .then(function (user) {
+                        to = ko.unwrap(user.Emel);
+                        return $.get("/email-template/generate/Permohonan/" + ko.unwrap(permohonan().Id) + "/peringatan-kepada-responden");
+                    })
+                    .then(function (mail) {
+                        var message = {
+                            subject: mail.subject,
+                            body: mail.body,
+                            to: to
+                        };
+                        return context.post(JSON.stringify(message), "/email-template/send");
+                    }).then(function () {
+                        app.showMessage("Emel peringatan sudah dihantar kepada " + to, config.applicationFullName, ["OK"]);
+                    });
 
             };
         };
-        
-        // process the list of students
-        filePelajar.subscribe(function(w){
-            context.post(JSON.stringify({id : w}), "/jpa-account/process-respondents")
-            .done(function(r){
-                console.log(r);
-            });
+
+    // process the list of students
+    filePelajar.subscribe(function (w) {
+        context.post(JSON.stringify({ id: w, permohonanId: ko.unwrap(permohonan().Id) }), "/pelajar/process-file")
+        .done(function (r) {
+            if (r.success) {
+                _(r.list).each(function (v) {
+                    senaraiPendaftaran.push(v);
+                });
+            }
         });
+    });
 
     return {
-        addResponden : addResponden,
-        senaraiPendaftaran : senaraiPendaftaran,
-        sendReminderEmail : sendReminderEmail,
-        filePelajar : filePelajar,
-        activate : activate,
-        attached : attached
+        addResponden: addResponden,
+        senaraiPendaftaran: senaraiPendaftaran,
+        sendReminderEmail: sendReminderEmail,
+        filePelajar: filePelajar,
+        activate: activate,
+        attached: attached
     };
 
 });
