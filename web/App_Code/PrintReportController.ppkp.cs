3a464033ed0f08c7ef358628a544fe31e1a7f596
erymuzuan <erymuzuan@hotmail.com>
2015-08-10 13:11:08 +0800

    PPKP indikator

--- web/App_Code/PrintReportController.ppkp.cs
+++ web/App_Code/PrintReportController.ppkp.cs
@@ -41,6 +41,35 @@ namespace web.sph.App_Code
             return View("Trait-Ppkp",vm);
 
         }
+        [Route("indikator/ppkp/{id}")]
+        public async Task<ActionResult> PrintIndikatorPpkp(string id)
+        {
+            var context = new SphDataContext();
+            var sesi = await context.LoadOneAsync<SesiUjian>(x => x.Id == id);
+            var user = await context.LoadOneAsync<Pengguna>(x => x.MyKad == sesi.MyKad);
+
+            var ujianTask = context.LoadOneAsync<Ujian>(x => x.Id == sesi.NamaUjian);
+            var permohonanTask = context.LoadOneAsync<Permohonan>(x => x.PermohonanNo == sesi.NamaProgram);
+            var recommendationTask = context.LoadAsync(context.CreateQueryable<PpkpRecommendation>());
+            await Task.WhenAll(ujianTask, permohonanTask, recommendationTask);
+
+            var rlo = await recommendationTask;
+
+            if (null == sesi)
+                return HttpNotFound("Cannot find SesiUjian " + id);
+            if (null == user)
+                return HttpNotFound("Cannot find user with MyKad " + sesi.MyKad);
+
+            var vm = new PpkpTraitViewModel(sesi, rlo.ItemCollection.ToArray())
+            {
+                Permohonan = await permohonanTask,
+                Ujian = await ujianTask,
+                Pengguna = user
+            };
+
+            return View("Indikator-Ppkp",vm);
+
+        }
 
 
 
