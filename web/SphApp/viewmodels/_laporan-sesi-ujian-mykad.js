dddf03572191fcd14e4a62514437fead1d5dd5a7
erymuzuan <erymuzuan@hotmail.com>
2015-08-26 08:08:41 +0800

    Gabungkan laporan UKBP dekat carian ikut MyKad di urusetia dashboard #20

--- web/SphApp/viewmodels/_laporan-sesi-ujian-mykad.js
+++ web/SphApp/viewmodels/_laporan-sesi-ujian-mykad.js
@@ -9,8 +9,26 @@ define(["services/datacontext"], function(context){
             $(view).on("submit", "form", function(e){
               e.preventDefault();
               context.loadAsync("SesiUjian", "MyKad eq '" + ko.unwrap(mykad) + "' and Status eq 'Diambil'")
-                .done(function(lo){
-                  results(lo.itemCollection);
+                .done(function (lo) {
+
+                    var sesi = [];
+                    _(lo.itemCollection).each(function (v) {
+                        var ujian = ko.unwrap(v.NamaUjian),
+                            indikator = !(ujian === "HLP" || ujian === "IBK" || ujian === "IP"),
+                            trait = !(ujian === "UKBP-A" || ujian === "UKBP-B"|| ujian === "UKBP");
+                        v.traitButton = ko.observable(trait);
+                        v.indikatorButton = ko.observable(indikator);
+                        if (ujian === "UKBP-A" || ujian === "UKBP-B") {
+
+                        } else {
+                            sesi.push(v);
+                        }
+                        if (ujian === "UKBP-A") {
+                            v.NamaUjian("UKBP");
+                            sesi.push(v);
+                        }
+                    });
+                  results(sesi);
                 });
             });
         };
