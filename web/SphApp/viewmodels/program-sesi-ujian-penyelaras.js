2684a24df385a56d40ba2baf5e802f069f4e76e6
erymuzuan <erymuzuan@hotmail.com>
2015-08-28 08:11:24 +0800

    Link dari program-sesi-ujian-penyelaras view href kepada permohonan-penyelaras-readonly

--- web/SphApp/viewmodels/program-sesi-ujian-penyelaras.js
+++ web/SphApp/viewmodels/program-sesi-ujian-penyelaras.js
@@ -9,185 +9,185 @@
 /// <reference path="../../Scripts/bootstrap.js" />
 
 
-define(["services/datacontext", "services/logger", "plugins/router", "services/chart", objectbuilders.config , "partial/permohonan-urusetia-lulus"],
-    function (context, logger, router, chart,config , partial) {
+define(["services/datacontext", "services/logger", "plugins/router", "services/chart", objectbuilders.config, "partial/permohonan-urusetia-lulus"],
 
-        var isBusy = ko.observable(false),
-            chartFiltered = ko.observable(false),
-            view = ko.observable(),
-            list = ko.observableArray([]),
-            map = function(v) {
-                if (typeof partial !== "undefined" && typeof partial.map === "function") {
-                    return partial.map(v);
-                }
-                return v;
-            },
-            entity = ko.observable(new bespoke.sph.domain.EntityDefinition()),
-            query = ko.observable(),
-            activate = function () {
-                query({
-                    "query": {
-                        "filtered": {
-                            "filter": {
-               "bool": {
-                  "must": [
-                                     {
-                     "term":{
-                         "StatusPermohonan":"LULUS"
-                     }
-                 }
-,
-                 {
-                     "term":{
-                         "Penyelaras":config.userName
-                     }
-                 }
+function(context, logger, router, chart, config, partial) {
 
-                  ],
-                  "must_not": [
-                    
-                  ]
-               }
-           }
+    var isBusy = ko.observable(false),
+        chartFiltered = ko.observable(false),
+        view = ko.observable(),
+        list = ko.observableArray([]),
+        map = function(v) {
+            if (typeof partial !== "undefined" && typeof partial.map === "function") {
+                return partial.map(v);
+            }
+            return v;
+        },
+        entity = ko.observable(new bespoke.sph.domain.EntityDefinition()),
+        query = ko.observable(),
+        activate = function() {
+            query({
+                "query": {
+                    "filtered": {
+                        "filter": {
+                            "bool": {
+                                "must": [{
+                                    "term": {
+                                        "StatusPermohonan": "LULUS"
+                                    }
+                                }, {
+                                    "term": {
+                                        "Penyelaras": config.userName
+                                    }
+                                }
+
+                                ],
+                                "must_not": [
+
+                                ]
+                            }
                         }
-                    },
-                    "sort" : []
+                    }
+                },
+                "sort": []
+            });
+            var edQuery = String.format("Name eq '{0}'", 'Permohonan'),
+                tcs = new $.Deferred(),
+                formsQuery = String.format("EntityDefinitionId eq 'permohonan' and IsPublished eq 1 and IsAllowedNewItem eq 1"),
+                viewQuery = String.format("EntityDefinitionId eq 'permohonan'"),
+                edTask = context.loadOneAsync("EntityDefinition", edQuery),
+                formsTask = context.loadAsync("EntityForm", formsQuery),
+                viewTask = context.loadOneAsync("EntityView", viewQuery);
+
+
+            $.when(edTask, viewTask, formsTask)
+                .done(function(b, vw, formsLo) {
+                entity(b);
+                view(vw);
+                var formsCommands = _(formsLo.itemCollection).map(function(v) {
+                    return {
+                        caption: v.Name(),
+                        command: function() {
+                            window.location = '#' + v.Route() + '/0';
+                            return Task.fromResult(0);
+                        },
+                        icon: v.IconClass()
+                    };
                 });
-                var edQuery = String.format("Name eq '{0}'", 'Permohonan'),
-                  tcs = new $.Deferred(),
-                  formsQuery = String.format("EntityDefinitionId eq 'permohonan' and IsPublished eq 1 and IsAllowedNewItem eq 1"),
-                  viewQuery = String.format("EntityDefinitionId eq 'permohonan'"),
-                  edTask = context.loadOneAsync("EntityDefinition", edQuery),
-                  formsTask = context.loadAsync("EntityForm", formsQuery),
-                  viewTask = context.loadOneAsync("EntityView", viewQuery);
+                vm.toolbar.commands(formsCommands);
 
 
-                $.when(edTask, viewTask, formsTask)
-                 .done(function (b, vw,formsLo) {
-                     entity(b);
-                     view(vw);
-                     var formsCommands = _(formsLo.itemCollection).map(function (v) {
-                         return {
-                             caption: v.Name(),
-                             command: function () {
-                                 window.location = '#' + v.Route() + '/0';
-                                 return Task.fromResult(0);
-                             },
-                             icon: v.IconClass()
-                         };
-                     });
-                     vm.toolbar.commands(formsCommands);
-
-                         
-                         if(typeof partial !== "undefined" && typeof partial.activate === "function"){
-                             var pt = partial.activate(list);
-                             if(typeof pt.done === "function"){
-                                 pt.done(tcs.resolve);
-                             }else{
-                                 tcs.resolve(true);
-                             }
-                         }
-                         
-
-                 });
-
-
-
-                return tcs.promise();
-            },
-            chartSeriesClick = function(e) {
-               
-                isBusy(true);
-                var q = ko.mapping.toJS(query),
-                    cat = {
-                        "term": {
-                        }
-                    },
-                    histogram = {
-                        "range": {
-                        }
-                    },
-                    date_histogram = {
-                        "range": {
-                        }
-                    };
-
-                if (e.aggregate === "histogram") {
-                    histogram.range[e.field] = {
-                        "gte": parseFloat(e.category),
-                        "lt": ( parseFloat(e.category) + e.query.aggs.category.histogram.interval )
-                    };
-
-                    q.query.filtered.filter.bool.must.push(histogram);
+                if (typeof partial !== "undefined" && typeof partial.activate === "function") {
+                    var pt = partial.activate(list);
+                    if (typeof pt.done === "function") {
+                        pt.done(tcs.resolve);
+                    } else {
+                        tcs.resolve(true);
+                    }
                 }
-                if (e.aggregate === "date_histogram") {
-                    logger.error('Filtering by date range is not supported just yet');
+
+
+            });
+
+
+
+            return tcs.promise();
+        },
+        chartSeriesClick = function(e) {
+
+            isBusy(true);
+            var q = ko.mapping.toJS(query),
+                cat = {
+                    "term": {}
+                },
+                histogram = {
+                    "range": {}
+                },
+                date_histogram = {
+                    "range": {}
+                };
+
+            if (e.aggregate === "histogram") {
+                histogram.range[e.field] = {
+                    "gte": parseFloat(e.category),
+                    "lt": (parseFloat(e.category) + e.query.aggs.category.histogram.interval)
+                };
+
+                q.query.filtered.filter.bool.must.push(histogram);
+            }
+            if (e.aggregate === "date_histogram") {
+                logger.error('Filtering by date range is not supported just yet');
+                isBusy(false);
+                return;
+                date_histogram.range[e.field] = {
+                    "gte": parseFloat(e.category),
+                    "lt": (parseFloat(e.category) + e.query.aggs.category.date_histogram.interval)
+                };
+
+                q.query.filtered.filter.bool.must.push(date_histogram);
+            }
+            if (e.aggregate === "term") {
+                if (e.category === "<Empty>") {
+                    var missing = {
+                        "missing": {
+                            "field": e.field
+                        }
+                    };
+                    q.query.filtered.filter.bool.must.push(missing);
+                } else {
+                    cat.term[e.field] = e.category;
+                    q.query.filtered.filter.bool.must.push(cat);
+                }
+            }
+
+
+
+            context.searchAsync("Permohonan", q)
+                .done(function(lo) {
+                list(lo.itemCollection);
+                chartFiltered(true);
+                setTimeout(function() {
                     isBusy(false);
-                    return;
-                    date_histogram.range[e.field] = {
-                        "gte": parseFloat(e.category),
-                        "lt": ( parseFloat(e.category) + e.query.aggs.category.date_histogram.interval )
-                    };
+                }, 500);
+            });
+        },
+        attached = function(view) {
+            chart.init("Permohonan", query, chartSeriesClick, "permohonan-program-sesi-ujian-penyelaras");
 
-                    q.query.filtered.filter.bool.must.push(date_histogram);
-                }
-                if(e.aggregate === "term"){  
-                    if(e.category === "<Empty>"){
-                        var missing = {"missing" : { "field" : e.field}};
-                        q.query.filtered.filter.bool.must.push(missing);
-                    }else {
-                        cat.term[e.field] = e.category;
-                        q.query.filtered.filter.bool.must.push(cat);
-                    }
-                }
+            if (typeof partial !== "undefined" && typeof partial.attached === "function") {
+                partial.attached(view);
+            }
 
-
-
-                context.searchAsync("Permohonan", q)
-                    .done(function (lo) {
-                        list(lo.itemCollection);
-                        chartFiltered(true);
-                        setTimeout(function () { isBusy(false); }, 500);
-                    });
-            },
-            attached = function (view) {
-                chart.init("Permohonan", query, chartSeriesClick, "permohonan-program-sesi-ujian-penyelaras");
-                    
-                    if(typeof partial !== "undefined" && typeof partial.attached === "function"){
-                        partial.attached(view);
-                    }
-                    
-            },
-            clearChartFilter = function(){
-                chartFiltered(false);
-                var link = $('div.k-pager-wrap a.k-link').not('a.k-state-disabled').first();
-                link.trigger('click');
-                if(link.text() === "2")
-                {
-                    setTimeout(function(){
-                        $('div.k-pager-wrap a.k-link').not('a.k-state-disabled').first().trigger('click');
-                    }, 500);
-                }
-            };
-
-        var vm = {
-            config: config,
-            view: view,
-            chart: chart,
-            isBusy: isBusy,
-            map: map,
-            entity: entity,
-            activate: activate,
-            attached: attached,
-            list: list,
-            clearChartFilter:clearChartFilter,
-            chartFiltered:chartFiltered,
-            query: query,
-            toolbar: {
-                commands: ko.observableArray([])
+        },
+        clearChartFilter = function() {
+            chartFiltered(false);
+            var link = $('div.k-pager-wrap a.k-link').not('a.k-state-disabled').first();
+            link.trigger('click');
+            if (link.text() === "2") {
+                setTimeout(function() {
+                    $('div.k-pager-wrap a.k-link').not('a.k-state-disabled').first().trigger('click');
+                }, 500);
             }
         };
 
-        return vm;
+    var vm = {
+        config: config,
+        view: view,
+        chart: chart,
+        isBusy: isBusy,
+        map: map,
+        entity: entity,
+        activate: activate,
+        attached: attached,
+        list: list,
+        clearChartFilter: clearChartFilter,
+        chartFiltered: chartFiltered,
+        query: query,
+        toolbar: {
+            commands: ko.observableArray([])
+        }
+    };
 
-    });
+    return vm;
+
+});
