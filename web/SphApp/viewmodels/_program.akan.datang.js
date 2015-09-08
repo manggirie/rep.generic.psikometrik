881df7fe6316120aab17816cbd94043d2e637b9e
erymuzuan <erymuzuan@hotmail.com>
2015-09-08 08:50:34 +0800

    Program akan datang #61

---
+++ web/SphApp/viewmodels/_program.akan.datang.js
@@ -0,0 +1,52 @@
+define(["services/datacontext", "services/logger", "plugins/router", "services/config"], function(context, logger, router, config){
+    var today = moment().format("YYYY-MM-DDTHH:mm:ss.SSS"),
+      query = {
+              "query": {
+                "filtered": {
+                  "filter": {
+                    "bool": {
+                      "must": [
+                        {
+                          "term": {
+                            "StatusPermohonan": "LULUS"
+                          }
+                        },
+                        {
+                          "term": {
+                            "Penyelaras": config.userName
+                          }
+                        },
+                        {
+                            "range": {
+                                "TarikhMula": {
+                                   "from": today
+                                }
+                             }
+                        }
+                      ],
+                      "must_not": []
+                    }
+                  }
+                }
+              },
+              "sort": []
+            },
+      permohonanLulusList = ko.observableArray(),
+      activate = function(entity){
+
+            return true;
+
+
+        },
+        attached  = function(view){
+
+        };
+
+    return {
+        activate : activate,
+        query: query,
+        permohonanLulusList: permohonanLulusList,
+        attached : attached
+    };
+
+});
