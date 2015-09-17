ac3533c945e1ad3af5fb8e194c5b88c4f2753fbe
Raja Mohd Kamil <rajakamil@bespoke.com.my>
2015-09-17 15:58:54 +0800

    update hrmis.adapter

--- sources/Hrmis.Adapter/HrmisAdapter.cs
+++ sources/Hrmis.Adapter/HrmisAdapter.cs
@@ -28,7 +28,7 @@ namespace Hrmis.Adapter
                     {
                         var pengguna = new Pengguna();
                         pengguna.Nama = reader["CONm"].ReadNullableString();
-                        pengguna.Umur = reader["Age"].ReadNullableString();
+                        pengguna.Umur = int.Parse(reader["Age"].ToString());
                         pengguna.Emel = reader["COEmail"].ReadNullableString();
                         pengguna.StatusPerkahwinan = reader["MrtlStatus"].ReadNullableString();
                         pengguna.Telefon = reader["COHPhoneNo"].ReadNullableString();
