38c3a3fe9715cbd469a4e9e0d32dab3cf62a94da
erymuzuan <erymuzuan@hotmail.com>
2015-09-29 08:22:07 +0800

    Double uriEncode for NamaUjian #99

--- web/SphApp/partial/sesi-ujian-jabatan-program.js
+++ web/SphApp/partial/sesi-ujian-jabatan-program.js
@@ -1,87 +1,88 @@
-define([objectbuilders.app, "services/datacontext", objectbuilders.config], function(app, context, config){
-    var map = function(v){
-            if(v.NamaUjian === "UKBP-A"){
-                v.NamaUjian = "UKBP";
-            }
-            if(v.NamaUjian === "UKBP-B"){
-                v.NamaUjian = "UKBP";
-            }
-            return v;
-        },
-        activate = function(list){
-            
+define([objectbuilders.app, "services/datacontext", objectbuilders.config], function (app, context, config) {
+    var map = function (v) {
+        if (v.NamaUjian === "UKBP-A") {
+            v.NamaUjian = "UKBP";
+        }
+        if (v.NamaUjian === "UKBP-B") {
+            v.NamaUjian = "UKBP";
+        }
+        return v;
+    },
+        activate = function (list) {
+
             var items = list(),
                 mapping = false;
-            list.subscribe(function(changes) {
-              
-                if(mapping){
+            list.subscribe(function (changes) {
+
+                if (mapping) {
                     return;
-                }  
-                
-                
-                
+                }
+
+
+
                 items = list();
                 mapping = true;
-                
+
                 var baskets = [],
                     filterdItems = [];
-                
-                setTimeout(function(){
-                    
-                    _(items).each(function(v){
+
+                setTimeout(function () {
+
+                    _(items).each(function (v) {
                         var key = v.NamaUjian + v.NamaProgram + v.MyKad;
-                        if(baskets.indexOf(key) === -1){
+                        if (baskets.indexOf(key) === -1) {
                             baskets.push(key);
                             filterdItems.push(v);
                         }
                     });
                     list(filterdItems);
                     mapping = false;
-                },250);
-                
+                }, 250);
+
             }, null, "arrayChange");
-            
+
             return true;
 
         },
-        sendReminderEmail = function(sesi, permohonanId){
-                var to = "";
-                return context.loadOneAsync("Pengguna", String.format("MyKad eq '{0}'", ko.unwrap(sesi.MyKad)))
-                    .then(function(user){
-                        to = ko.unwrap(user.Emel);
-                        return $.get("/email-template/generate/Permohonan/" + ko.unwrap(permohonanId) + "/peringatan-kepada-responden") ;
-                    })
-                    .then(function(mail){
-                        var message = {
-                            subject : mail.subject,
-                            body : mail.body,
-                            to : to
-                        };
-                       return context.post(JSON.stringify(message), "/email-template/send");
-                    }).then(function(){
-                        app.showMessage("Emel peringatan sudah dihantar kepada " + to, config.applicationFullName, ["OK"]);
-                    });
+        sendReminderEmail = function (sesi, permohonanId) {
+            var to = "";
+            return context.loadOneAsync("Pengguna", String.format("MyKad eq '{0}'", ko.unwrap(sesi.MyKad)))
+                .then(function (user) {
+                    to = ko.unwrap(user.Emel);
+                    return $.get("/email-template/generate/Permohonan/" + ko.unwrap(permohonanId) + "/peringatan-kepada-responden");
+                })
+                .then(function (mail) {
+                    var message = {
+                        subject: mail.subject,
+                        body: mail.body,
+                        to: to
+                    };
+                    return context.post(JSON.stringify(message), "/email-template/send");
+                }).then(function () {
+                    app.showMessage("Emel peringatan sudah dihantar kepada " + to, config.applicationFullName, ["OK"]);
+                });
 
 
-            },
-        attached  = function(view){
-            $(view).on("click", "a", function(e){
-              var data = ko.dataFor(this);
-              if(typeof data.MyKad !== "string"){
-                return;
-              }
-              // do the email
-              return context.getScalarAsync("Permohonan","PermohonanNo eq '" + encodeURIComponent(data.NamaProgram) + "'", "Id")
-              .then(function(permohonanId){
-                return sendReminderEmail(data, permohonanId);
-              });
+        },
+        attached = function (view) {
+            $(view).on("click", "a", function (e) {
+                var data = ko.dataFor(this);
+                if (typeof data.MyKad !== "string") {
+                    return 0;
+                }
+                e.preventDefault();
+                // do the email
+                return context.getScalarAsync("Permohonan", "PermohonanNo eq '" + data.NamaProgram + "'", "Id")
+                .then(function (permohonanId) {
+                    return sendReminderEmail(data, permohonanId);
+                });
             });
         };
 
     return {
-        activate : activate,
-        attached : attached,
-        map : map
+        activate: activate,
+        attached: attached,
+        map: map
     };
 
 });
