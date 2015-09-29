755181ae02626d5a5414671394f518449f93b89b
erymuzuan <erymuzuan@hotmail.com>
2015-09-29 08:04:31 +0800

    redirect to the correct report for different type of Ujian #100

--- web/SphApp/partial/sesi-ujian-diambil-by-program.js
+++ web/SphApp/partial/sesi-ujian-diambil-by-program.js
@@ -1,30 +1,41 @@
-define([], function(){
-    var activate = function(list){
+define([], function () {
+    var activate = function (list) {
 
-            var tcs = new $.Deferred();
-            setTimeout(function(){
-                tcs.resolve(true);
-            }, 500);
+        var tcs = new $.Deferred();
+        setTimeout(function () {
+            tcs.resolve(true);
+        }, 500);
 
-            return tcs.promise();
+        return tcs.promise();
 
 
-        },
-        attached  = function(view){
-          $(view).on("click", "a", function(e){
-            var data = ko.dataFor(this);
-            if(typeof data.NamaUjian !== "string"){
-              return;
-            }
+    },
+        attached = function (view) {
+            $(view).on("click", "a", function (e) {
+                var data = ko.dataFor(this);
+                if (typeof data.NamaUjian !== "string") {
+                    return;
+                }
 
-            e.preventDefault();
-            window.open("/cetak-laporan/trait/" + data.NamaUjian +"/" + data.Id);
-          });
+                e.preventDefault();
+                var url = "/cetak-laporan/trait/" + data.NamaUjian + "/" + data.Id;
+                switch (data.NamaUjian) {
+                    case "PPKP":
+                        url = "/cetak-laporan/ppkp/profile/" + data.Id;
+                        break;
+                    case "UKBP-A":
+                    case "UKBP-B":
+                        url = "/cetak-laporan/indikator/ukbp/" + data.Id;
+                        break;
+                    default:
+                }
+                window.open(url);
+            });
         };
 
     return {
-        activate : activate,
-        attached : attached
+        activate: activate,
+        attached: attached
     };
 
 });
