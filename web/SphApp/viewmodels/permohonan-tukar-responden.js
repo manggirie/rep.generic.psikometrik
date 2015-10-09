faecc3440d187cd586851e4de7b5c3840e70c04f
unknown <Administrator@psikometrik.jpa.gov.my>
2015-10-09 15:58:10 +0800

    no message

--- web/SphApp/viewmodels/permohonan-tukar-responden.js
+++ web/SphApp/viewmodels/permohonan-tukar-responden.js
@@ -173,7 +173,7 @@
                     return context.post(data, "/Permohonan/Save")
                         .then(function(result) {
                             entity().Id(result.id);
-                            app.showMessage("Permohonan berjaya dihantar", "JPA Sistem Ujian e-Psikometrik", ["OK"]);
+                            app.showMessage("Your Permohonan has been successfully saved", "JPA Sistem Ujian e-Psikometrik", ["OK"]);
 
                         });
                     
