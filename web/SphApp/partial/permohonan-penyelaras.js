161c18e6a52e71331eda0d9bd3775b3f74296536
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2015-10-19 13:12:04 +0800

    #148 tukar upper case kepada title case

--- web/SphApp/partial/permohonan-penyelaras.js
+++ web/SphApp/partial/permohonan-penyelaras.js
@@ -1,44 +1,47 @@
-define([objectbuilders.config], function(config){
-    var     permohonan = ko.observable(),
-            activate = function(entity){
+define([objectbuilders.config], function (config) {
+
+    function toTitleCase(str) {
+        return str.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+    }
+
+    var permohonan = ko.observable(),
+            activate = function (entity) {
                 permohonan(entity);
                 entity.NamaJabatan(config.namaJabatan);
                 entity.NamaKementerian(config.namaKementerian);
                 entity.Penyelaras(config.userName);
-                if(!ko.unwrap(entity.StatusPermohonan)){
+                if (!ko.unwrap(entity.StatusPermohonan)) {
                     entity.StatusPermohonan("BARU");
                 }
-                
-                ko.extenders.uppercase = function(target, option) {
-                target.subscribe(function(newValue) {
-                target(newValue.toUpperCase());
-                });
-                return target;
+
+                ko.extenders.toTitleCase = function (target, option) {
+                    target.subscribe(function (newValue) {
+                        target(toTitleCase(newValue));
+                    });
+                    return target;
                 };
 
-                entity.NamaProgram.extend({ uppercase: true });
+                entity.NamaProgram.extend({ toTitleCase: true });
 
                 return true;
-    
-    
             },
-            
-            
-            attached  = function(view){
+
+
+            attached = function (view) {
                 var tarikhMulaPicker = $("#tarikhMulaPicker").data("kendoDatePicker"),
                     tarikhTamatPicker = $("#tarikhTamatPicker").data("kendoDatePicker");
                 tarikhMulaPicker.min(new Date());
                 tarikhTamatPicker.min(new Date());
-                
+
                 tarikhMulaPicker.enable(permohonan().StatusPermohonan() === 'BARU');
                 tarikhTamatPicker.enable(permohonan().StatusPermohonan() === 'BARU');
-                
-                $("NamaProgram").css("text-transform","capitalize");
+
+                $("NamaProgram").css("text-transform", "capitalize");
             };
 
     return {
-        activate : activate,
-        attached : attached
+        activate: activate,
+        attached: attached
     };
 
 });
