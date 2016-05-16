e881d902d1304aed6e9c8b507643d319de071813
Ruzzaima Kamaruldzaman <ruzzaima@hotmail.com>
2016-05-16 12:00:03 +0800

    tambah maklumat agensi pada senarai program #185

---
+++ web/SphApp/viewmodels/sesiujian-details.js
@@ -0,0 +1,144 @@
+
+    define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router,
+        objectbuilders.system, objectbuilders.validation, objectbuilders.eximp,
+        objectbuilders.dialog, objectbuilders.watcher, objectbuilders.config,
+        objectbuilders.app ],
+        function (context, logger, router, system, validation, eximp, dialog, watcher,config,app
+            ) {
+
+            var entity = ko.observable(new bespoke.epsikologi_sesiujian.domain.SesiUjian({WebId:system.guid()})),
+                errors = ko.observableArray(),
+                form = ko.observable(new bespoke.sph.domain.EntityForm()),
+                watching = ko.observable(false),
+                id = ko.observable(),
+                i18n = null,
+                activate = function (entityId) {
+                    id(entityId);
+
+                    var query = String.format("Id eq '{0}'", entityId),
+                        tcs = new $.Deferred(),
+                        itemTask = context.loadOneAsync("SesiUjian", query),
+                        formTask = context.loadOneAsync("EntityForm", "Route eq 'sesiujian-details'"),
+                        watcherTask = watcher.getIsWatchingAsync("SesiUjian", entityId),
+                        i18nTask = $.getJSON("i18n/" + config.lang + "/sesiujian-details");
+
+                    $.when(itemTask, formTask, watcherTask, i18nTask).done(function(b,f,w,n) {
+                        if (b) {
+                            var item = context.toObservable(b);
+                            entity(item);
+                        }
+                        else {
+                            entity(new bespoke.epsikologi_sesiujian.domain.SesiUjian({WebId:system.guid()}));
+                        }
+                        form(f);
+                        watching(w);
+                        i18n = n[0];
+                            tcs.resolve(true);
+                        
+                    });
+
+                    return tcs.promise();
+                },
+                submitSesiUjian = function(){
+
+                     if (!validation.valid()) {
+                         return Task.fromResult(false);
+                     }
+
+                     var data = ko.mapping.toJSON(entity);
+
+                    return  context.post(data, "/SesiUjian/SubmitSesiUjian" )
+                         .then(function (result) {
+                             if (result.success) {
+                                 logger.info(result.message);
+                                 entity().Id(result.id);
+                                 errors.removeAll();
+
+                                 
+                             } else {
+                                 errors.removeAll();
+                                 _(result.rules).each(function(v){
+                                     errors(v.ValidationErrors);
+                                 });
+                                 logger.error("There are errors in your entity, !!!");
+                             }
+                         });
+                 },
+                attached = function (view) {
+                    // validation
+                    validation.init($('#sesiujian-details-form'), form());
+
+
+
+                },
+                compositionComplete = function() {
+                    $("[data-i18n]").each(function (i, v) {
+                        var $label = $(v),
+                            text = $label.data("i18n");
+                        if (typeof i18n[text] === "string") {
+                            $label.text(i18n[text]);
+                        }
+                    });
+                },
+
+                                save = function() {
+                    if (!validation.valid()) {
+                        return Task.fromResult(false);
+                    }
+
+                    var data = ko.mapping.toJSON(entity);
+
+                        
+
+                    return context.post(data, "/SesiUjian/Save")
+                        .then(function(result) {
+                            entity().Id(result.id);
+                            app.showMessage("Your SesiUjian has been successfully saved", "JPA Sistem Ujian e-Psikometrik", ["OK"]);
+
+                        });
+                    
+
+                },
+                remove = function() {
+                    return $.ajax({
+                        type: "DELETE",
+                        url: "/SesiUjian/Remove/" + entity().Id(),
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function() {
+                            app.showMessage("Your item has been successfully removed", "Removed", ["OK"])
+                              .done(function () {
+                                  window.location = "#sesiujian";
+                              });
+                        }
+                    });
+
+
+                };
+
+            var vm = {
+                                    activate: activate,
+                config: config,
+                attached: attached,
+                compositionComplete:compositionComplete,
+                entity: entity,
+                errors: errors,
+                save : save,
+                    submitSesiUjian : submitSesiUjian,
+                //
+
+
+                toolbar : {
+                                                        removeCommand :remove,
+                    canExecuteRemoveCommand : ko.computed(function(){
+                        return entity().Id();
+                    }),
+                                                                
+                    saveCommand : save,
+                    
+                    commands : ko.observableArray([])
+                }
+            };
+
+            return vm;
+        });
